20005495260439c1bafd2194509a040206bb3703
My AD <Steven@n2-154-24.dhcp.drexel.edu>
2015-05-27 12:23:43 -0400

    Initial Commit

---
+++ HiC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HiC++
+//
+//  Created by My AD on 5/27/15.
+//  Copyright (c) 2015 My AD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
